0f28fdada91bb9db109ed87b039173bf504377b1
Yuri007 <136291503+YuriKauan10@users.noreply.github.com>
2024-12-28 19:56:25 -0300

    inicio projeto

---
+++ app/components/Amarelo.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Amarelo(){
+    return (
+        <div className="bg-yellow-300 w-96 h-36 m-auto text-center mt-16">
+
+            <div className="flex flex-col items-center gap-10 h-full imagem">
+
+           <div className="text-slate-900 fonteBold espacamento p-2 amarelo w-full">
+                EXTRA
+           </div>
+
+           <div className="text-slate-900 border-2 border-solid fonteBold border-black w-1/3 " >
+                12 Meses
+           </div>
+           </div>
+
+
+        </div>
+    )
+}
